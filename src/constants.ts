85b83db65e069fd825e8abe818562ef13c40d0c2
Chin-yu Hsu <chinyu.hsu@imersive.com>
2023-02-09 18:25:54 +0800

    Update regex pattern

--- src/constants.ts
+++ src/constants.ts
@@ -1,11 +1,24 @@
 const PASSWORD_RULES: { message: string; pattern: RegExp }[] = [
-  { message: "Have at least one uppercase letter", pattern: /[A-Z]/ },
-  { message: "Have at least one lowercase letter", pattern: /[a-z]/ },
-  { message: "Have at least one number", pattern: /\d/ },
+  {
+    message: "Have at least one uppercase letter",
+    pattern: /(?=.*[A-Z])/,
+  },
+  {
+    message: "Have at least one lowercase letter",
+    pattern: /(?=.*[a-z])/,
+  },
+  {
+    message: "Have at least one number",
+    pattern: /(?=.*\d)/,
+  },
   {
     message: "Have at least one special character (!@#$...etc)",
-    pattern: /[^(a-zA-z\w\s)]/,
+    // pattern: /(?=.*[!@#\$%\^&\*])/,
+    pattern: /[!@#$%^&*]+/,
   },
-  { message: "Longer than 8 characters", pattern: /.{8,}/ },
+  {
+    message: "Longer than 8 characters",
+    pattern: /(?=.{8,})/,
+  },
 ];
 export { PASSWORD_RULES };
